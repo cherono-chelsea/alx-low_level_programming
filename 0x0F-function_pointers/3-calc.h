9d043b4f43125fb67bc1c246531c906bacf93a7a
cherono-chelsea <cheronochelsea@gmail.com>
2023-11-08 16:53:20 -0800

    simple operations

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,5 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef CALC_H
+#define CALC_H
 
 /**
  * struct op - Struct op
@@ -19,4 +19,5 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
+
 #endif
